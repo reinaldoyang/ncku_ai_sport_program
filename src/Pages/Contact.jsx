669368624d171a5dbfa9a0aba03eff46ca03e658
Reinaldo Yang <reinaldoyang5@gmail.com>
2025-11-18 14:03:40 +0800

    removed lab assistant from contact pages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -20,13 +20,6 @@ export function Contact() {
                 <td>81818</td>
                 <td><a href="mailto:z11208119@ncku.edu.tw">z11208119@ncku.edu.tw</a></td>
                 </tr>
-                <tr>
-                <td>黃如薇</td>
-                <td>資工程學系</td>
-                <td>專案工作人員</td>
-                <td>62530#2601</td>
-                <td><a href="mailto:z11407316@ncku.edu.tw">z11407316@ncku.edu.tw</a></td>
-                </tr>
             </tbody>
         </table>
 
